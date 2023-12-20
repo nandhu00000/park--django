a4d2f4f4aaa10856be17ccfe1745f8b1955c44f0
nandhu00000 <nandha00pro@gmail.com>
2023-12-19 14:37:03 -1200

    respone

--- my_project/settings.py
+++ my_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'my_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['C:\Users\NANDHA___PRO\Desktop\dd--jango\my_project\cdio\templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
